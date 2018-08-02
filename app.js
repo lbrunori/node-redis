eaf642213ef3657d47a97f87817b3d4971609c80
Lucas Brunori <lucas.brunori@gmail.com>
2018-08-02 14:25:20 -0300

    Finished with basic functionality project

--- app.js
+++ app.js
@@ -26,11 +26,76 @@ app.get("/", (req, res, next) => {
   let title = "Task list";
 
   client.lrange("tasks", 0, -1, (err, reply) => {
-    res.render("index", {
-      title,
-      tasks: reply
+    client.hgetall("call", (err, call) => {
+      res.render("index", {
+        title,
+        tasks: reply,
+        call
+      });
     });
   });
+});
+
+app.post("/task/add", (req, res, next) => {
+  let task = req.body.task;
+
+  client.rpush("tasks", task, (err, reply) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("Task added");
+  });
+
+  res.redirect("/");
+});
+
+app.post("/task/delete", (req, res, next) => {
+  let taskToDel = req.body.task;
+
+  client.lrange("tasks", 0, -1, (err, tasks) => {
+    console.log("tasks ->" + tasks);
+
+    for (let i = 0; i < tasks.length; i++) {
+      if (taskToDel.indexOf(tasks[i]) > -1) {
+        client.lrem("tasks", 0, tasks[i], () => {
+          if (err) {
+            console.log(err);
+          }
+        });
+      }
+    }
+    res.redirect("/");
+  });
+});
+
+app.post("/call/add", (req, res, next) => {
+  let newCall = {
+    name: req.body.name,
+    company: req.body.company,
+    phone: req.body.phone,
+    time: req.body.time
+  };
+
+  client.hmset(
+    "call",
+    [
+      "name",
+      newCall.name,
+      "company",
+      newCall.company,
+      "phone",
+      newCall.phone,
+      "time",
+      newCall.time
+    ],
+    (err, reply) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(reply);
+      res.redirect("/");
+    }
+  );
 });
 
 app.listen(3000);
